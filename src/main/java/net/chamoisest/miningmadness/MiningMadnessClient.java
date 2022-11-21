f592616b59328b007a3d399b02148042acdf90af
Ivar Äkke <ivar@1kdigital.com>
2022-11-22 00:12:08 +0200

    Initial commit

---
+++ src/main/java/net/chamoisest/miningmadness/MiningMadnessClient.java
@@ -0,0 +1,10 @@
+package net.chamoisest.miningmadness;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MiningMadnessClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
